4a24081017acf261ec938d349ed6963d5533e6f9
huangjinnan_mac <hjn755423541@gmail.com>
2023-10-14 14:55:13 +0800

    Fix path.

--- run_data_processing_workflow.py
+++ run_data_processing_workflow.py
@@ -10,16 +10,19 @@
 
 from data_processing_workflow import DataProcessingWorkflow
 
+NUM_CHUNKS = 25
+
 workflow = DataProcessingWorkflow(
     session_range=(15, 100),
     min_max_num_clicked_cards=5,
     min_num_sessions_with_clicks=8,
-    sorted_data_file="sorted_scroll_added_data.csv",
-    ranges_file="user_ranges_num_chunks_50.pkl",
-    num_chunks=50,
+    sorted_data_file="./sorted_scroll_added_data.csv",
+    ranges_file=f"./user_ranges_num_chunks_{NUM_CHUNKS}.pkl",
+    num_chunks=NUM_CHUNKS,
     subsample_size=4000,
     subsample_seed=42,
-    save_data_before_subsampling=True
+    save_data_before_subsampling=True,
+    overwrite_ranges=True
 )
 
 workflow.run()
